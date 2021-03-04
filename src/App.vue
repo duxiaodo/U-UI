1b9eb561598a34ba1d8a3ef550405e5c2e3ce45f
duxiaodo <2088867510@qq.com>
2021-03-04 21:09:39 +0800

    input

--- src/App.vue
+++ src/App.vue
@@ -47,15 +47,55 @@
 				>加载中</o-button
 			>
 		</div>
+		<div>
+					<o-button-group>
+			<o-button icon="left" position="left">上一页</o-button><o-button icon="right" position="right" >下一页</o-button>
+		</o-button-group>
+		<o-button-group>
+			<o-button icon="delete" position="left" ></o-button><o-button icon="update" position="right" ></o-button><o-button icon="share" position="left" ></o-button>
+		</o-button-group>
+		</div>
+				<div>
+			<h4>基础用法</h4>
+			<o-input></o-input>
+		</div>
+        <div>
+			<h4>disabled</h4>
+			<o-input disabled></o-input>
+		</div>
+        <div>
+			<h4>readonly</h4>
+			<o-input readonly></o-input>
+		</div>
+        <div>
+			<h4>带 icon 的输入框</h4>
+            <o-input prefix-icon="update" class="item" v-model="searchText" @input="handleInput"></o-input>
+			<o-input suffix-icon="info"></o-input>
+		</div>
 	</div>
 </template>
 <script>
 import OButton from './components/general/Button'
+import OButtonGroup from './components/general/ButtonGroup'
+import OInput from './components/form/Input'
+
 
 export default {
 	components: {
 		OButton,
+		OButtonGroup,
+		OInput
 	},
+	data() {
+		return {
+			searchText: 'v-model'
+		}
+	},
+	methods: {
+		handleInput() {
+			console.log(this.searchText)
+		}
+	}
 }
 </script>
 <style lang="scss" scoped>
---
+++ src/components/form/Input.vue
@@ -0,0 +1,114 @@
+<template>
+	<div
+		class="o-input"
+		:class="{
+			'suffix-icon': suffixIcon,
+			'prefix-icon': prefixIcon,
+		}"
+	>
+		<input
+			:value="value"
+			:type="type"
+			:readonly="readonly"
+			:disabled="disabled"
+			:placeholder="placeholder"
+			@input="$emit('input', $event.target.value)"
+			@change="$emit('change', $event.target.value)"
+			@blur="$emit('blur', $event.target.value)"
+			@focus="$emit('focus', $event.target.value)"
+		/>
+		<icon
+			:type="suffixIcon ? suffixIcon : prefixIcon"
+			size="small"
+			:style="{ color: '#999' }"
+		></icon>
+	</div>
+</template>
+<script>
+import Icon from '../general/Icon';
+
+export default {
+  components: {
+    Icon, 
+  },
+  props: {
+    value: String,
+    type: {
+      type: String,
+      default: 'text',
+    },
+    disabled: Boolean,
+    placeholder: {
+      type: String,
+      default: '请输入内容。。。',
+    },
+    readonly: Boolean,
+    'suffix-icon': String,
+    'prefix-icon': String,
+  },
+  data() {
+    return {};
+  },
+};
+</script>
+<style lang="scss" scoped>
+$height: 32px;
+
+.o-input {
+	display: inline-flex;
+	align-items: center;
+	position: relative;
+	height: $height;
+	line-height: $height;
+
+	input {
+		font-size: 12px;
+		height: inherit;
+		line-height: inherit;
+		padding: 0 10px;
+		outline: none;
+		color: #606266;
+		cursor: pointer;
+		border: 1px solid #dcdfe6;
+
+		&:focus {
+			border-color: #1989fa;
+		}
+
+		&:hover {
+			box-shadow: 0 0 1px 0 #333;
+		}
+
+		&:disabled {
+			cursor: not-allowed;
+			box-shadow: none;
+		}
+
+		&:read-only {
+			border-color: #999;
+			box-shadow: none;
+		}
+	}
+}
+
+.suffix-icon {
+	input {
+		padding: 0 30px 0 10px;
+	}
+
+	i {
+		position: absolute;
+		right: 10px;
+	}
+}
+
+.prefix-icon {
+	input {
+		padding: 0 10px 0 30px;
+	}
+	i {
+		position: absolute;
+		left: 10px;
+	}
+}
+</style>
