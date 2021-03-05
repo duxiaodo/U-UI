0ae326ff522cf6f6e2e85972962cb18459f22f65
duxiaodo <2088867510@qq.com>
2021-03-05 23:24:39 +0800

    重构

--- src/plugins/toastPlugin.js
+++ src/plugins/toastPlugin.js
@@ -1,14 +1,25 @@
 import Toast from '../components/other/Toast'
 
+let curToast
+
 export default {
     install(Vue, options) {
         Vue.prototype.$toast = function(propOptions) {
-            let Constructor = Vue.extend(Toast);
-            let toast = new Constructor({
-                propsData: propOptions
-            })
-            toast.$mount()
-            document.body.appendChild(toast.$el);
+            if(curToast) {
+                curToast.$el.remove()
+                curToast.$destroy()
+            }
+            curToast = createToast(Vue, propOptions)
         }
     }
+}
+
+function createToast(Vue, propOptions) {
+    let Constructor = Vue.extend(Toast);
+    let toast = new Constructor({
+        propsData: propOptions
+    })
+    toast.$mount()
+    document.body.appendChild(toast.$el);
+    return toast
 }
